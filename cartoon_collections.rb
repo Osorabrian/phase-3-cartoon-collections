9589075532290e3890a5e908b139d66cf7cb6693
Osorabrian <osorabrian@gmail.com>
2023-02-07 22:26:44 +0300

    completed assignment

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,20 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves arr
+  puts arr.map.with_index{|cartoon, index| "#{index+1}. #{cartoon}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet arr
+    arr.map{|cartoon| "#{cartoon.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls arr
+  if arr.filter{|call| call.length > 4}.length > 0
+    return true
+  end
+    false
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese arr
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  arr.find{|cheese| cheese_types.include?(cheese)}
 end
